f63f63026019b885e01f15df99861f6a2dc242f3
Beatriz Bernardo <BeatrizBernardo@users.noreply.github.com>
2021-04-05 23:10:18 +0100

    clean code

--- src/DisplaySearch.js
+++ src/DisplaySearch.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Audio from "./Audio.js";
 import "./css/DisplaySearch.css";
 
 export default function DisplaySearch(props) {
@@ -11,36 +12,16 @@ export default function DisplaySearch(props) {
         {"  "}•{"  "}
         {props.meaning.text}
         {"  "}•{"  "}
-        <button
-          className="btn sound"
-          onClick={function () {
-            const audio = document.getElementsByClassName("audioFile")[0];
-            audio.play();
-          }}
-        >
-          Listen{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-soundwave"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5zm-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5zm12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5z"
-            />
-          </svg>
-        </button>
-        <audio className="audioFile" src={props.meaning.audio}></audio>
+        <Audio audio={props.meaning.audio} />
       </h3>
       <h2 className="subtitle">Definitions</h2>
       <hr className="secondHorizontalLine" />
-      <p>
-        <strong>definition: </strong>
-        {props.meaning.definition}
-      </p>
+      {props.meaning.definition && (
+        <p>
+          <strong>definition: </strong>
+          {props.meaning.definition}
+        </p>
+      )}
       {props.meaning.example && (
         <p>
           <strong>example: </strong>
